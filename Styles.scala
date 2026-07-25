1409255b59761a1e34276183c1157e4c01f4521f
bjornregnell <bjorn.regnell@cs.lth.se>
2026-07-25 12:07:48 +0200

    add stubb for Stykes todo: honor design language

---
+++ Styles.scala
@@ -0,0 +1,8 @@
+package prontopop
+
+object Styles:
+  def createPageStyle: Unit = ???  // should return a <style> html element
+    //TODO: should honor the genscalator's design language: see 
+    //https://github.com/bjornregnell/genscalator/tree/main/media/design-language
+    //local clone: ~/git/hub/bjornregnell/genscalator/media/design-language
+    // live at: https://bjornregnell.se/genscalator/design-language/index.html
